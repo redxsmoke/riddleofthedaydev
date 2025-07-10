7fe34a2f56ff70cd9417bff2137b30aab4411b63
redxsmoke <andrewevans9560@gmail.com>
2025-07-10 16:16:06 -0400

    sad

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def riddle_announcement():
     await channel.send(embed=embed)
 
 
-@tasks.loop(seconds=120)
+@tasks.loop(seconds=60)
 async def daily_riddle_post():
     global current_riddle, current_answer_revealed, correct_users, guess_attempts, deducted_for_user
 
@@ -329,7 +329,7 @@ async def daily_riddle_post():
 
 
 
-@tasks.loop(seconds=3600)
+@tasks.loop(seconds=90)
 async def reveal_riddle_answer():
     global current_riddle, current_answer_revealed, correct_users, guess_attempts, deducted_for_user
 
